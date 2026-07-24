23fa322c27633ddeba88b11cd8abc8d4d7292528
MartinVerniere <mn.verniere@gmail.com>
2026-07-24 18:59:29 -0300

    Added task-list component

---
+++ frontend/src/app/tasks/task-list/task-list.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input, output, signal } from '@angular/core';
+import { Task } from '../../services/columns/column-service';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../../services/tasks/task-service';
+
+@Component({
+	selector: 'app-task-list',
+	imports: [],
+	templateUrl: './task-list.html',
+	styleUrl: './task-list.css',
+})
+export class TaskList {
+	route = inject(ActivatedRoute);
+	boardService = inject(TaskService);
+
+	projectId = Number(this.route.snapshot.paramMap.get('projectId'));
+	boardId = Number(this.route.snapshot.paramMap.get('boardId'));
+	columnId = Number(this.route.snapshot.paramMap.get('columnId'));
+
+	taskList = input.required<Task[]>;
+
+	taskMoved = output<void>();
+	taskDeleted = output<void>();
+
+	error = signal<string | null>(null);
+
+	onMoveUp(taskId: number) { }
+
+	onMoveDown(taskId: number) { }
+
+	onRemoveTask() {
+		this.taskDeleted.emit();
+	}
+}
