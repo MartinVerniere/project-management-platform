d50712ac997138a39f8fd16b7b2d7e54a4931fb9
MartinVerniere <mn.verniere@gmail.com>
2026-07-24 19:14:24 -0300

    Added task-service

---
+++ frontend/src/app/services/tasks/task-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TaskModel } from '../../tasks/task-form/task-form';
+
+const API_URL = 'http://localhost:3000/api/tasks';
+
+export interface Task {
+	id: number,
+	title: string,
+	description?: string,
+}
+
+@Service()
+export class TaskService {
+	private http = inject(HttpClient);
+
+	getTask(taskId: number): Observable<Task> {
+		return this.http.get<Task>(`${API_URL}/${taskId}`);
+	}
+
+	updateTask(taskId: number, request: TaskModel): Observable<Task> {
+		return this.http.put<Task>(`${API_URL}/${taskId}`, request);
+	}
+
+	deleteTask(taskId: number): Observable<void> {
+		return this.http.delete<void>(`${API_URL}/${taskId}`);
+	}
+}
