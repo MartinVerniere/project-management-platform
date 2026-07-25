f8f4b6b1f8a663d3ae2879a33da3c056197566bc
MartinVerniere <mn.verniere@gmail.com>
2026-07-25 20:46:11 -0300

    Move deleteProject to project-element

--- frontend/src/app/projects/project-element/project-element.ts
+++ frontend/src/app/projects/project-element/project-element.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
-import { Project } from '../../services/projects/project-service';
+import { Component, inject, input, output, signal } from '@angular/core';
+import { Project, ProjectService } from '../../services/projects/project-service';
 import { RouterLink } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
 	selector: 'app-project-element',
@@ -9,5 +10,24 @@ import { RouterLink } from '@angular/router';
 	styleUrl: './project-element.css',
 })
 export class ProjectElement {
+	projectService = inject(ProjectService);
+
 	project = input.required<Project>();
+	projectDeleted = output<void>();
+
+	error = signal<string | null>(null);
+
+	onDeleteProject(projectId: number) {
+		this.projectService.deleteProject(projectId).subscribe({
+			next: () => {
+				this.projectDeleted.emit();
+				this.error.set(null);
+			},
+			error: (response: HttpErrorResponse) => {
+				const errorObject = response.error.error;
+				console.log(errorObject);
+				this.error.set(errorObject.message);
+			}
+		});
+	}
 }
