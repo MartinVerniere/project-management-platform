9cb7181c7c3c6028679577907756521b3a0f90a1
MartinVerniere <mn.verniere@gmail.com>
2026-08-01 18:17:25 -0300

    Now sets assignee with a button

--- frontend/src/app/tasks/task-element/task-element.ts
+++ frontend/src/app/tasks/task-element/task-element.ts
@@ -28,11 +28,21 @@ export class TaskElement {
 	members = resource({ loader: () => firstValueFrom(this.projectService.getMembers(this.projectId())) });
 	memberList = computed(() => { return this.members.value() ?? [] });
 
+	selectedAssigneeId = signal<number | null>(null);
 	assigneeFormEnabled = signal<boolean>(false);
 	error = signal<string | null>(null);
 
 	onEnableSetAssignee() { this.assigneeFormEnabled.set(true); }
-	onCancelSetAssignee() { this.assigneeFormEnabled.set(false); }
+	onAssigneeChanged(event: Event) {
+		const target = event.target as HTMLSelectElement;
+		const value = target.value;
+
+		this.selectedAssigneeId.set(value === 'null' ? null : parseInt(value));
+	}
+	onCancelSetAssignee() { 
+		this.selectedAssigneeId.set(null);
+		this.assigneeFormEnabled.set(false); 
+	}
 
 	onTaskDeleted(taskId: number) {
 		this.taskService.deleteTask(taskId).subscribe({
@@ -48,15 +58,11 @@ export class TaskElement {
 		});
 	}
 
-	onAssigneeChanged(event: Event) {
-		event.preventDefault();
-		
-		const value = (event.target as HTMLSelectElement).value;
-		const userId = value === '' ? null : Number(value);
-
-		if (userId === null) {
+	onSetAssignee() {
+		if (this.selectedAssigneeId() === null) {
 			this.taskService.unassignTask(this.task().id).subscribe({
 				next: () => {
+					this.selectedAssigneeId.set(null);
 					this.assigneeFormEnabled.set(false);
 					this.error.set(null);
 					this.taskAssigneeEdited.emit();
@@ -68,8 +74,9 @@ export class TaskElement {
 				}
 			});
 		} else {
-			this.taskService.assignTask(this.task().id, userId).subscribe({
+			this.taskService.assignTask(this.task().id, this.selectedAssigneeId()!).subscribe({
 				next: () => {
+					this.selectedAssigneeId.set(null);
 					this.assigneeFormEnabled.set(false);
 					this.error.set(null);
 					this.taskAssigneeEdited.emit();
