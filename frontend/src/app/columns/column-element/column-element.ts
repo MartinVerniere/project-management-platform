0b45fcc61ad69eb076f71e1564b6b7cdf841fb13
MartinVerniere <mn.verniere@gmail.com>
2026-08-06 19:59:58 -0300

    Refactored columns component UI

--- frontend/src/app/columns/column-element/column-element.ts
+++ frontend/src/app/columns/column-element/column-element.ts
@@ -5,10 +5,11 @@ import { TaskList } from "../../tasks/task-list/task-list";
 import { ProjectMemberResponse } from "../../models/project";
 import { ColumnService } from "../../services/columns/column-service";
 import { ColumnResponse } from "../../models/column";
+import { CdkDragHandle } from "@angular/cdk/drag-drop";
 
 @Component({
 	selector: 'app-column-element',
-	imports: [RouterLink, TaskList],
+	imports: [RouterLink, TaskList, CdkDragHandle],
 	templateUrl: './column-element.html',
 	styleUrl: './column-element.css',
 })
