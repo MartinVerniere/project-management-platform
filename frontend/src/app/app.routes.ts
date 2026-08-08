edaad916aa2299062fcc9b15a0d80c2b1a9e5c0e
MartinVerniere <mn.verniere@gmail.com>
2026-08-08 10:33:19 -0300

    Now navbar appears only correct pages
    - No longer appears in authentication pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -13,13 +13,9 @@ import { ProjectForm } from "./projects/project-form/project-form";
 import { ProjectList } from "./projects/project-list/project-list";
 import { TaskUpdateForm } from "./tasks/task-update-form/task-update-form";
 import { TaskForm } from "./tasks/task-form/task-form";
+import { AppLayout } from "./layout/app-layout/app-layout";
 
 export const routes: Routes = [
-	{
-		path: '',
-		component: Home,
-		canActivate: [authGuard]
-	},
 	{
 		path: 'login',
 		component: Login
@@ -29,43 +25,54 @@ export const routes: Routes = [
 		component: Register
 	},
 	{
-		path: 'projects',
-		component: ProjectList
-	},
-	{
-		path: 'projects/create',
-		component: ProjectForm
-	},
-	{
-		path: 'projects/:id',
-		component: ProjectDetails
-	},
-	{
-		path: 'projects/:id/boards/create',
-		component: BoardForm,
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId',
-		component: BoardDetails
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId/edit',
-		component: BoardUpdateForm
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId/columns/create',
-		component: ColumnForm
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId/columns/:columnId/edit',
-		component: ColumnUpdateForm
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId/columns/:columnId/tasks/create',
-		component: TaskForm
-	},
-	{
-		path: 'projects/:projectId/boards/:boardId/columns/:columnId/tasks/:taskId/edit',
-		component: TaskUpdateForm
+		path: '',
+		component: AppLayout,
+		canActivate: [authGuard],
+		children: [
+			{
+				path: '',
+				component: Home
+			},
+			{
+				path: 'projects',
+				component: ProjectList
+			},
+			{
+				path: 'projects/create',
+				component: ProjectForm
+			},
+			{
+				path: 'projects/:id',
+				component: ProjectDetails
+			},
+			{
+				path: 'projects/:id/boards/create',
+				component: BoardForm
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId',
+				component: BoardDetails
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId/edit',
+				component: BoardUpdateForm
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId/columns/create',
+				component: ColumnForm
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId/columns/:columnId/edit',
+				component: ColumnUpdateForm
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId/columns/:columnId/tasks/create',
+				component: TaskForm
+			},
+			{
+				path: 'projects/:projectId/boards/:boardId/columns/:columnId/tasks/:taskId/edit',
+				component: TaskUpdateForm
+			}
+		]
 	}
 ];
