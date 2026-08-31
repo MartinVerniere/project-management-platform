962def3e76c929aa98f7fb2cb6c908a1e82d28d7
MartinVerniere <mn.verniere@gmail.com>
2026-08-31 17:26:12 -0300

    Added test for toast component

--- frontend/src/app/toast/toast.spec.ts
+++ frontend/src/app/toast/toast.spec.ts
@@ -1,22 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Toast } from './toast';
+import { ToastService } from '../services/toast/toast-service';
 
 describe('Toast', () => {
-  let component: Toast;
-  let fixture: ComponentFixture<Toast>;
+	let component: Toast;
+	let fixture: ComponentFixture<Toast>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Toast],
-    }).compileComponents();
+	let toastServiceMock = {
+		toasts: vi.fn(),
+		remove: vi.fn()
+	};
 
-    fixture = TestBed.createComponent(Toast);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+	async function createComponent(shouldAwait = true) {
+		fixture = TestBed.createComponent(Toast);
+		component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture.detectChanges();
+
+		if (shouldAwait) {
+			await fixture.whenStable();
+			fixture.detectChanges();
+		};
+	};
+
+	function setDefaultReturnValues() {
+		toastServiceMock.toasts = vi.fn().mockReturnValue([
+			{
+				id: 1,
+				message: 'Test toast',
+				type: 'success'
+			}
+		]);
+		toastServiceMock.remove.mockReturnValue({});
+	};
+
+	beforeEach(async () => {
+		vi.clearAllMocks();
+
+		setDefaultReturnValues();
+
+		await TestBed.configureTestingModule({
+			imports: [Toast],
+			providers: [
+				{ provide: ToastService, useValue: toastServiceMock },
+			],
+		}).compileComponents();
+	});
+
+	it('should create', async () => {
+		await createComponent();
+
+		expect(component).toBeTruthy();
+	});
+
+	it('should remove toast on cancel button click', async () => {
+		await createComponent();
+
+		const button = fixture.nativeElement.querySelector('.toast button') as HTMLButtonElement;
+		button.click();
+
+		expect(toastServiceMock.remove).toHaveBeenCalledWith(1);
+	});
 });
