7e8a0502c9f979bb20cfa459ccbb20c4e7b68302
MartinVerniere <mn.verniere@gmail.com>
2026-09-03 18:40:06 -0300

    Refactored style of navbar UI

--- frontend/src/app/navbar/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar/navbar.ts
@@ -1,10 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../services/auth/auth-service';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
-	imports: [RouterLink],
+	imports: [RouterLink, RouterLinkActive],
 	templateUrl: './navbar.html',
 	styleUrl: './navbar.css',
 })
